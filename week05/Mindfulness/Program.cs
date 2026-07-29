3db374c31cc264852703df09ab3f58f4c8699634
benjaminewoh <benjaminfridayewoh18@gmail.com>
2026-07-29 03:52:20 +0100

    Complete W05 Mindfulness Program

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,9 +1,51 @@
-using System;
+﻿using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        bool running = true;
+
+        while (running)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start Breathing Activity");
+            Console.WriteLine("  2. Start Reflection Activity");
+            Console.WriteLine("  3. Start Listing Activity");
+            Console.WriteLine("  4. Quit");
+            Console.Write("\nSelect a choice from the menu: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    BreathingActivity breathing = new BreathingActivity();
+                    breathing.Run();
+                    break;
+
+                case "2":
+                    ReflectionActivity reflection = new ReflectionActivity();
+                    reflection.Run();
+                    break;
+
+                case "3":
+                    ListingActivity listing = new ListingActivity();
+                    listing.Run();
+                    break;
+
+                case "4":
+                    running = false;
+                    Console.WriteLine("\nThank you for using the Mindfulness Program!");
+                    break;
+
+                default:
+                    Console.WriteLine("\nInvalid choice. Press Enter to continue...");
+                    Console.ReadLine();
+                    break;
+            }
+        }
     }
 }
